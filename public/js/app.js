e2962b7d4ba3cb5d9c6543fd320221ca989751a7
Agilulfe <guillaume.jacobs1995@outlook.com>
2020-09-14 15:04:23 +0200

    reduce the amount of pokemon to 100

--- public/js/app.js
+++ public/js/app.js
@@ -9,8 +9,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function () { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -36,36 +36,38 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var container = document.getElementById("app");
-var pokemons = 150;
+var pokemons = 100;
 var fetchData = function () {
     for (var i = 1; i <= pokemons; i++) {
         getPokemon(i);
     }
 };
-var getPokemon = function (id) { return __awaiter(void 0, void 0, void 0, function () {
-    var data, pokemon, pokemonType, transformedPokemon;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, fetch("https://pokeapi.co/api/v2/pokemon/" + id)];
-            case 1:
-                data = _a.sent();
-                return [4 /*yield*/, data.json()];
-            case 2:
-                pokemon = _a.sent();
-                pokemonType = pokemon.types
-                    .map(function (poke) { return poke.type.name; })
-                    .join(", ");
-                transformedPokemon = {
-                    id: pokemon.id,
-                    name: pokemon.name,
-                    image: "" + pokemon.sprites.front_default,
-                    type: pokemonType,
-                };
-                showPokemon(transformedPokemon);
-                return [2 /*return*/];
-        }
+var getPokemon = function (id) {
+    return __awaiter(void 0, void 0, void 0, function () {
+        var data, pokemon, pokemonType, transformedPokemon;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, fetch("https://pokeapi.co/api/v2/pokemon/" + id)];
+                case 1:
+                    data = _a.sent();
+                    return [4 /*yield*/, data.json()];
+                case 2:
+                    pokemon = _a.sent();
+                    pokemonType = pokemon.types
+                        .map(function (poke) { return poke.type.name; })
+                        .join(", ");
+                    transformedPokemon = {
+                        id: pokemon.id,
+                        name: pokemon.name,
+                        image: "" + pokemon.sprites.front_default,
+                        type: pokemonType,
+                    };
+                    showPokemon(transformedPokemon);
+                    return [2 /*return*/];
+            }
+        });
     });
-}); };
+};
 var showPokemon = function (pokemon) {
     var output = "\n        <div class=\"card\">\n            <span class=\"card--id\">#" + pokemon.id + "</span>\n            <img class=\"card--image\" src=" + pokemon.image + " alt=" + pokemon.name + " />\n            <h1 class=\"card--name\">" + pokemon.name + "</h1>\n            <span class=\"card--details\">" + pokemon.type + "</span>\n        </div>\n    ";
     container.innerHTML += output;
